a6710ce68496837cfde99007c784dbef011310d5
guobao2333 <125613427+guobao2333@users.noreply.github.com>
2025-09-18 04:15:10 +0800

    docs: 添加Android SDK安装文档

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
 # MT Plugin
+<p align="center">
+<a href="https://github.com/guobao2333/MT-Plugin"><img alt="Repository" src="https://img.shields.io/badge/Github-%230A0A0A.svg?&style=flat-square&logo=Github&logoColor=white"/></a>
+</p>
 
 适用于[MT管理器](https://mt2.cn)的插件，为MT添加更多扩展功能。
-> MT目前仅支持翻译接口，所以只有翻译插件。
+> MT目前仅支持翻译接口，所以只有翻译插件。  
+> 好消息是 MT 2.19.1 提供了更多接口，但目前处于开发状态，正式版暂时无福消受 |･_･`)
 
-基于[官方项目](https://github.com/L-JINBIN/MT-Translation-Plugin)修改而来，但由于修改的太多 ~~*已经面目全非了*~~，所以几乎可以算脱离了吧 (?)
+基于[官方项目](https://github.com/L-JINBIN/MT-Translation-Plugin)修改而来。最新[插件V3版本](https://mt2.cn/guide/plugin/introduction.html#v3-%E7%89%88%E6%9C%AC)仅发布至gitee但暂未同步github，由于打包逻辑已变，且几乎无法自定义，所以暂不考虑同步。
 
 ## Develop | 开发
 > [!IMPORTANT]
-> 有时插件代码在MT正式版中无法使用，推荐切换至测试版。
+> 部分插件使用V3版本，目前在MT正式版(2.19.0)中无法使用，推荐切换至测试版进行开发。
 
 所有插件都放 `plugins` 这个目录下，要增加新的插件直接丢进去就行了，可以随便复制一个项目内的模块级配置，不需要动任何项目级配置。
 
 ## Prerequisites | 基本要求
 1. Java 8 (可以但不推荐更旧版本)
-2. Gradle 14 (按需选择，可降级)
+2. Gradle 14 (按需选择，推荐最新版)
 
 按照以下方式运行命令后，将自动打包文件到 `outputs/` 这个目录中。
 
 ### Android Studio
-在 Android Studio 上方选择 `BuildPlugin` 并点击运行按钮，也可以在项目根目录运行
+找到 `BuildPlugin` 并点击运行按钮，也可以在项目根目录运行：
 ```bash
 ./gradlew buildPlugin
 ```
@@ -29,6 +33,9 @@
 chmod +x gradlew
 ./gradlew buildPlugin
 ```
+
+#### Setup termux development environment
+如果你是一个初学者，不知道如何为你的termux设置插件的开发环境，请看[这篇教程](docs/AndroidSDK.md)。不过由于部分命令的局限性，它并不能适用于其他Linux系统，您需要修改其中termux的特有命令和环境路径以适用您的系统。
 
 ## Contribute | 贡献
 1. 点击上方`fork`仓库后，修改或添加你的代码
---
+++ docs/install-android-sdk.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+if [ "$1" = "-h" ]; then
+    echo "欢迎使用自动设置Android SDK CLI Tool的脚本 (by 是果宝呐)"
+    echo "以下是可用参数："
+    echo "\t\t[DOWNLOAD_DIR: path]\t\t可选项，命令行工具的存放路径，默认'/sdcard/Download'"
+    exit 0
+else
+    # 设置下载的文件目录
+    DOWNLOAD_DIR=${1:-"/sdcard/Download"}
+fi
+
+
+########### 初始化 ###########
+echo "如果你已经授予存储权限可以输入n"
+termux-setup-storage
+echo "正在初始化依赖..."
+pkg update -y && pkg upgrade -y
+pkg install -y curl unzip git openjdk-17
+
+if [ ! -d "$DOWNLOAD_DIR" ]; then
+  echo "错误：无法访问 $DOWNLOAD_DIR"
+  exit 1
+fi
+
+######## 下载CLI tool #########
+# 技术有限，还请自行手动下载
+# URL="https://developer.android.com/studio#command-line-tools-only"
+# URL_CN="https://developer.android.google.cn/studio#command-line-tools-only"
+
+########## 设置shell ##########
+echo "请选择你的Shell以准确配置："
+echo "1) Ash"
+echo "2) Bash (默认)"
+echo "3) Dash"
+echo "4) Fish"
+echo "5) Zsh"
+echo "6) 其他"
+read -p "输入数字选择: " shell_choice
+
+case "$shell_choice" in
+    1)
+        shellrc=".ashrc"
+        ;;
+    2|"")
+        shellrc=".bashrc"
+        ;;
+    3)
+        shellrc=".dashrc"
+        ;;
+    4)
+        shellrc=".fishrc"
+        ;;
+    5)
+        shellrc=".zshrc"
+        ;;
+    6)
+        read -p "请输入你的Shell配置文件（小写，例如 .bashrc）: " custom_shell
+        shellrc="${custom_shell:-.bashrc}"  # 默认使用.bashrc
+        ;;
+    *)
+        echo "无效操作，默认使用Bash"
+        shellrc=".bashrc"
+        ;;
+esac
+
+
+######## 安装CLI tool #########
+echo "正在解压Android SDK..."
+ANDROID_SDK_DIR="$HOME/android_sdk"
+
+ZIP_FILE=$(ls "$DOWNLOAD_DIR"/commandlinetools-linux-*.zip | head -1)
+if [ -z "$ZIP_FILE" ]; then
+  echo -e "错误：未找到压缩包，请前往国区官网 \033[4mhttps://developer.android.google.cn/studio#command-line-tools-only\033[0m 或外区官网 \033[4mhttps://developer.android.com/studio#command-line-tools-only\033[0m 下载Linux版本压缩包"
+  exit 1
+fi
+unzip "$ZIP_FILE" -d "$ANDROID_SDK_DIR"
+if [ $? -ne 0 ]; then
+  echo "错误：解压失败！"
+  exit 1
+fi
+rm -v "$ZIP_FILE"
+echo "解压完成！已删除压缩包，正在移动文件..."
+
+mkdir -p "$ANDROID_SDK_DIR/cmdline-tools/latest"
+shopt -s extglob
+mv "$ANDROID_SDK_DIR"/cmdline-tools/!(latest) "$ANDROID_SDK_DIR/cmdline-tools/latest"
+
+
+######### 设置环境变量 #########
+echo "正在设置环境变量..."
+
+# 临时导出
+export ANDROID_HOME="$ANDROID_SDK_DIR"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
+# 输出配置
+echo 'export ANDROID_HOME="$HOME/android_sdk"' >> "$HOME/$shellrc"
+echo 'export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools' >> "$HOME/$shellrc"
+
+echo "同意所有许可并安装平台工具..."
+chmod -R 755 $ANDROID_HOME
+yes | sdkmanager --licenses "platform-tools"
+
+# 安装adb
+echo "需要安装adb工具吗？[y/N]"
+read -r adbtool
+if [[ "$adbtool" =~ ^[Yy]$ ]]; then
+    pkg install -y android-tools
+fi
+
+# 创建local.properties
+echo "是否创建local.properties文件？[y/N]"
+read -r answer
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+    echo "输入文件保存路径（默认为当前目录）："
+    read -r path
+    if [ -z "$path" ]; then
+        path="."
+    fi
+    mkdir -p "$path"
+    echo "sdk.dir=$ANDROID_SDK_DIR" > "$path/local.properties"
+    echo "已创建 $path/local.properties 文件"
+fi
+
+echo "Android SDK 安装完成！位于：$ANDROID_SDK_DIR"
+echo "请重启终端或执行 source ~/$shellrc 使配置生效"
