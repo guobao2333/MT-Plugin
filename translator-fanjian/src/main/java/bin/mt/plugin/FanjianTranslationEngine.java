9e1dd9299096c2519932a7e1940c2624a60a974b
L-JINBIN <921558445@qq.com>
2026-02-21 16:16:28 +0800

    新增translator-fanjian

---
+++ translator-fanjian/src/main/java/bin/mt/plugin/FanjianTranslationEngine.java
@@ -0,0 +1,53 @@
+package bin.mt.plugin;
+
+import androidx.annotation.NonNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import bin.mt.plugin.api.translation.BaseTranslationEngine;
+
+/**
+ * 繁简互转
+ *
+ * @author Bin
+ */
+public class FanjianTranslationEngine extends BaseTranslationEngine {
+    private final List<String> sourceLanguages = Arrays.asList("zh-CN", "zh-TW");
+    private final List<String> targetLanguages1 = Collections.singletonList("zh-TW");
+    private final List<String> targetLanguages2 = Collections.singletonList("zh-CN");
+
+    public FanjianTranslationEngine() {
+        super(new ConfigurationBuilder()
+                .setTargetLanguageMutable(true)
+                .setDisableAutoHideLanguage(true)
+                .build()
+        );
+    }
+
+    @NonNull
+    @Override
+    public String name() {
+        return "{plugin_name}";
+    }
+
+    @NonNull
+    @Override
+    public List<String> loadSourceLanguages() {
+        return sourceLanguages;
+    }
+
+    @NonNull
+    @Override
+    public List<String> loadTargetLanguages(String sourceLanguage) {
+        return sourceLanguage.equals("zh-CN") ? targetLanguages1 : targetLanguages2;
+    }
+
+    @NonNull
+    @Override
+    public String translate(String text, String sourceLanguage, String targetLanguage) {
+        return FanjianTranslator.translate(text, sourceLanguage, targetLanguage);
+    }
+
+}
