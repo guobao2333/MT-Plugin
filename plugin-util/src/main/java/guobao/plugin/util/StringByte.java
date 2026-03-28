c79828ab95a41d0a03885664ab4085eb91efa6d9
guobao2333 <shiguobaona@163.com>
2026-03-28 16:08:41 +0800

    增加工具类

---
+++ plugin-util/src/main/java/guobao/plugin/util/StringByte.java
@@ -0,0 +1,15 @@
+package guobao.plugin.util;
+
+import java.io.*;
+
+public class StringByte {
+    // String → byte[]
+    public static byte[] toByte(String str, String encoding) throws UnsupportedEncodingException {
+        return str.getBytes(encoding);
+    }
+
+    // byte[] → String
+    public static String toString(byte[] bytes, String encoding) throws UnsupportedEncodingException {
+        return new String(bytes, encoding);
+    }
+}
